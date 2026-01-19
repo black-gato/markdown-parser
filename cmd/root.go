136fba9781e4667efdeacd7a5a5ab220dfd2642a
black-gato <black-gato@ss.com>
2026-01-19 16:59:24 -0600

    change short name

--- cmd/root.go
+++ cmd/root.go
@@ -15,7 +15,7 @@ var logger *log.Logger
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "parser",
-	Short: "A brief description of your application",
+	Short: "this is a test",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
 
